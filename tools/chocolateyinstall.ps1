8b6177087f21720b34d440ddb1372f9fa0ba9484
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-05-02 11:09:05 +0200

    initial version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://gallery.technet.microsoft.com/scriptcenter/Reset-Windows-Update-Agent-d824badc/file/224689/1/ResetWUEng.zip'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$(Join-Path $toolsDir ResetWUEng)"
+  url           = $url
+  softwareName  = 'reset-windows-update-tool*'
+  checksum      = '4899B5EDE5434005BF692F3729B57A6E1524BC46629A7263FF6EABBD5F741198'
+  checksumType  = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
